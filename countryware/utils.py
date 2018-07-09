d91d617057890ecdc9f9e38e51d2a3cb181af7f0
Val Neekman <val@neekware.com>
2018-07-09 10:31:52 -0400

    clean up

--- countryware/utils.py
+++ countryware/utils.py
@@ -4,43 +4,47 @@ from django.core.cache import cache
 from django.utils import translation
 from django.utils.translation import ugettext as _
 
-_cache_enabled = None
+_cache_backend_enabled = None
 
 
 def check_cache():    
     """ Check if a cache backend is available """
-    global _cache_enabled
-    if _cache_enabled is None:
+    global _cache_backend_enabled
+    if _cache_backend_enabled is None:
         value = 100
         key = 'testing-if-cache-is-enabled'
         try:
             cache.set(key, value)
             if cache.get(key) == value:
-                _cache_enabled = True
+                _cache_backend_enabled = True
         except Exception:
-            _cache_enabled = False
+            _cache_backend_enabled = False
 
 
 def get_from_cache(backend, key):
-    if _cache_enabled is True:
+    """ get value for key from cache """
+    if _cache_backend_enabled is True:
         return cache.get(key)
     return backend.get(key)
 
 
 def set_to_cache(backend, key, value):
-    if _cache_enabled is True:
+    """ set value for key to cache """
+    if _cache_backend_enabled is True:
         cache.set(key, value)
     else:
         backend[key] = value
 
 
 def get_cache_key(backend):
+    """ get cache key for operation """
     lang = translation.get_language()
     key = '{}-{}'.format(id(backend), lang)
     return key
 
 
 def memorize(cache_storage):
+    """ decorator to cache operation """
     def decorator(function):
         def wrapper(*args, **kwargs):
             check_cache()
